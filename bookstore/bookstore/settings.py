1ad366098dc534239faf50ad080e12645e7d65a4
Anirudh <60171262+Blank333@users.noreply.github.com>
2023-10-10 20:06:11 +0530

    Removed DRF pagination

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -130,13 +130,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-## User confs ##
-
-# Rest framework
-# Response page size
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
