5bc023452fd7305b82fe6b7a7f6acdebdac63080
Anirudh <60171262+Blank333@users.noreply.github.com>
2023-10-10 19:50:47 +0530

    Added documentation

--- bookstore/book/serializers.py
+++ bookstore/book/serializers.py
@@ -8,3 +8,9 @@ class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
+
+    def validate_price(self, value):
+        if value < 0:
+            raise serializers.ValidationError("Price cannot be negative.")
+
+        return value
--- bookstore/book/views.py
+++ bookstore/book/views.py
@@ -3,6 +3,8 @@ from .models import Book
 from .serializers import BookSerializer
 
 
+# ModelViewSet includes implementations for CRUD Operations
+# It provides .list(), .retrieve(), .create(), .update(), .partial_update(), and .destroy()
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
