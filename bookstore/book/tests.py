2f36414c52346b189742a6d8f805ec025aa793eb
Anirudh <60171262+Blank333@users.noreply.github.com>
2023-10-10 20:04:19 +0530

    Added more comprehensive test cases
    Added documentation for test cases

--- bookstore/book/tests.py
+++ bookstore/book/tests.py
@@ -20,12 +20,15 @@ class BookTestCaseCreate(APITestCase):
         self.url = '/api/v1/books/'
 
     # Testing book object creation
+
+    # Verifies if a book can be successfully created
     def test_create_book(self):
         response = self.client.post(self.url, self.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Book.objects.count(), 1)
         self.assertEqual(Book.objects.get().title, 'Test Book')
 
+    # Verifies that creating a book without a title, author or price is not possible
     def test_create_book_without_title(self):
         data = self.data
         data.pop('title')
@@ -38,6 +41,13 @@ class BookTestCaseCreate(APITestCase):
         response = self.client.post(self.url, self.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_create_book_without_price(self):
+        data = self.data
+        data.pop('price')
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # Verifies that creating a book without a genre is possible
     def test_create_book_without_genre(self):
         data = self.data
         data.pop('genre')
@@ -46,15 +56,16 @@ class BookTestCaseCreate(APITestCase):
         self.assertEqual(Book.objects.get().title, 'Test Book')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_create_book_without_price(self):
-        data = self.data
-        data.pop('price')
-        response = self.client.post(self.url, self.data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
+    # Verifies that creating a book with a negative price, non-integer price, or a high price (>9999.99) is not possible
     def test_create_book_with_negative_price(self):
         data = self.data
         data['price'] = -10
+        response = self.client.post(self.url, self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_book_with_high_price(self):
+        data = self.data
+        data['price'] = 10000
         response = self.client.post(self.url, self.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -82,16 +93,20 @@ class BookTestCaseRead(APITestCase):
         self.url = '/api/v1/books/'
 
     # Testing books read
+
+    # Verifies if books returns  the correct number of books
     def test_retrieve_book(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(Book.objects.count(), 4)
 
+    # Verifies that retrieving details of a book by ID works
     def test_retrieve_book_detail(self):
         response = self.client.get(f'{self.url}{self.book.pk}/')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    # Verifies that retrieving a non-existent book is not possible
     def test_retrieve_book_detail_not_exsist(self):
         response = self.client.get(f'{self.url}1000/')
 
@@ -111,6 +126,8 @@ class BookTestCaseUpdate(APITestCase):
         self.url = '/api/v1/books/'
 
     # Testing book update
+
+    # Verifies if a book can be successfully updated with new data
     def test_update_book(self):
         updated_data = {
             "title": "Updated Book Title",
@@ -125,6 +142,7 @@ class BookTestCaseUpdate(APITestCase):
         self.assertEqual(Book.objects.get().author, 'Updated Author')
         self.assertEqual(Book.objects.get().genre, 'Updated Genre')
 
+    # Verifies that updating a book without a title, author or price is not possible
     def test_update_book_without_title(self):
         updated_data = {
             "title": "",
@@ -135,6 +153,26 @@ class BookTestCaseUpdate(APITestCase):
         response = self.client.put(f'{self.url}{self.book.pk}/', updated_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_update_book_without_author(self):
+        updated_data = {
+            "title": "Updated Book Title",
+            "author": "",
+            "genre": "Updated Genre",
+            "price": 14.99
+        }
+        response = self.client.put(f'{self.url}{self.book.pk}/', updated_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_book_without_price(self):
+        updated_data = {
+            "title": "Updated Book Title",
+            "author": "Updated Author",
+            "genre": "Updated Genre",
+        }
+        response = self.client.put(f'{self.url}{self.book.pk}/', updated_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # Verifies that updating a non-existent book is not possible
     def test_update_book_not_exsist(self):
         response = self.client.put(f'{self.url}1000/')
 
@@ -153,11 +191,15 @@ class BookTestCaseDelete(APITestCase):
 
         self.url = '/api/v1/books/'
 
+    # Testing book deletion
+
+    # Verifies if a book can be successfully deleted
     def test_delete_book(self):
         response = self.client.delete(f'{self.url}{self.book.pk}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
 
+    # Verifies that trying to delete a non-existent book is not possible
     def test_delete_book_not_exsist(self):
         response = self.client.delete(f'{self.url}1000/')
 
